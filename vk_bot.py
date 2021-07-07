b775e3dfa78d0cfc926049bde70a13eb5457212e
v1ztep <v1ztep@gmail.com>
2021-07-07 16:08:18 +0300

    Добавил эхо бота ВК

---
+++ vk_bot.py
@@ -0,0 +1,28 @@
+import random
+import os
+from dotenv import load_dotenv
+import vk_api as vk
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
+
+
+def main():
+    load_dotenv()
+    vk_token = os.getenv('VK_BOT_TOKEN')
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
+
+
+if __name__ == "__main__":
+    main()
