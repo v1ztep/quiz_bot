f3b3b4f3adcbe22347bc09bf64d4199d4aef0a23
v1ztep <v1ztep@gmail.com>
2021-07-10 00:16:08 +0300

    Добавил удаление мусора из вопросов и разбиение блитц-вопросов

--- get_questions.py
+++ get_questions.py
@@ -1,4 +1,5 @@
 import json
+import re
 from pathlib import Path
 
 
@@ -27,12 +28,38 @@ def clear_text(text):
         strip()
 
 
-def del_pic(quiz_questions_with_pic):
-    clean_quiz_questions = {}
-    for question, answer in quiz_questions_with_pic.items():
-        if '(pic:' not in question and '(pic:' not in answer:
-            clean_quiz_questions[question] = answer
-    return clean_quiz_questions
+def del_trash(questions_with_trash):
+    out_of_trash_questions = {}
+    for question, answer in questions_with_trash.items():
+        question_without_remark = re.sub(r'\[.*\]', '', question)
+        answer_without_remark = re.sub(r'\[.*\]', '', answer)
+        if '(pic:' in question or '(pic:' in answer or '<раздатка>' in question \
+                or '.jpg' in question or '.jpg' in answer:
+            continue
+        elif 'Блиц' in question_without_remark:
+            blitz_questions = split_blitz(question_without_remark, answer_without_remark)
+            out_of_trash_questions.update(blitz_questions)
+        else:
+            out_of_trash_questions[question_without_remark] = answer_without_remark
+    return out_of_trash_questions
+
+
+def split_blitz(questions, answers):
+    digits_with_dot_re = r'\b\d{1,2}\b\.'
+    only_blitz_questions = questions.split('Блиц')[1].strip()
+    entry = list(map(str.strip, re.split(digits_with_dot_re, only_blitz_questions)[:1]))[0]
+
+    split_questions = list(map(str.strip, re.split(digits_with_dot_re, only_blitz_questions)[1:]))
+    split_answers = list(map(str.strip, re.split(digits_with_dot_re, answers)[1:]))
+    questions_difference = len(split_questions) != len(split_answers)
+    if questions_difference or not split_questions:
+        return {}
+    if entry:
+        entry_questions = [entry + question for question in split_questions]
+        blitz_questions = dict(zip(entry_questions, split_answers))
+    else:
+        blitz_questions = dict(zip(split_questions, split_answers))
+    return blitz_questions
 
 
 def main():
@@ -43,9 +70,10 @@ def main():
         new_questions = get_quiz_questions(questions_path)
         questions.update(new_questions)
 
-    clean_quiz_questions = del_pic(questions)
+    out_of_trash_questions = del_trash(questions)
+
     with open('quiz_questions.json', 'w', encoding='utf8') as file:
-        json.dump(clean_quiz_questions, file, ensure_ascii=False, indent=4)
+        json.dump(out_of_trash_questions, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
