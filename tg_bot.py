6cc72aec82ced56c8e9a13db6dd99580a10aa071
v1ztep <v1ztep@gmail.com>
2021-07-18 21:43:49 +0300

    Оставил коммент по сложной конструкции check_similarity

--- tg_bot.py
+++ tg_bot.py
@@ -77,6 +77,7 @@ def check_answer(update: Update, context: CallbackContext) -> int:
 def check_similarity(user_answer, right_answer):
     similarity_check = fuzz.WRatio(
         user_answer.lower().strip(),
+        # Для сверки оставляю ответ без пояснений - идущих после скобки\точки.
         right_answer.split('(')[0].split('.')[0].lower().strip()
     )
     return similarity_check >= 80
--- vk_bot.py
+++ vk_bot.py
@@ -74,7 +74,8 @@ def check_answer(event, vk_api, quiz_questions, db):
 def similarity_check(user_answer, right_answer):
     similarity_check = fuzz.WRatio(
         user_answer.lower().strip(),
-        right_answer.split('(')[0].split('.')[0]. lower().strip()
+        # Для сверки оставляю ответ без пояснений - идущих после скобки\точки.
+        right_answer.split('(')[0].split('.')[0].lower().strip()
     )
     return similarity_check >= 80
 
